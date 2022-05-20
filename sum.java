759ab0947bf78999dc7db6517d22a5f2e9c91dd4
OMAL S <95520329+omals@users.noreply.github.com>
2022-05-20 14:49:33 +0530

    Day_1

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        int operand1 = 5;
+        int operand2 = 5;
+        int sum;
+        sum = operand1 + operand2;
+        System.out.println("Sum = "+sum); 
+    }
+}
---
+++ sum_userinput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        Scanner kb =new Scanner(System.in);
+        System.out.print("Enter the operand 1 : ");
+        int operand1 = kb.nextInt();
+        System.out.print("Enter the operand 2 : ");
+        int operand2 = kb.nextInt();
+        int sum;
+        sum = operand1 + operand2;
+        System.out.println("Sum = "+sum); 
+    }
+}
